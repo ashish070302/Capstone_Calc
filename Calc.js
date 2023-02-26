74aeb2d87de29d0b983d7dcfc8af37fe2db5cdb7
unknown <akshatnagori1750@gmail.com>
2023-02-27 00:14:26 +0530

    added js

--- Calc.js
+++ Calc.js
@@ -1,60 +1,17 @@
-const actions = document.querySelector('.actions');
-const ans = document.querySelector('.ans');
-console.log(actions);
-console.log(ans);
-let expression = '';
-let a=0;
-actions.addEventListener('click', (e) => {
-console.log(e.target);
-const value = e.target.dataset['value'];
-
-if(value !== undefined) {
-	// I'm good to go.
-	if(value == 'ce') {
-		expression = '';
-		ans.value = 0;
-		return true;
-	}
-	else if(value == 'x^2'){
-		expression =square();
-	}
-
-	else if(value == 'radic'){
-		expression = Math.sqrt(expression);
-	}
-	else if(value == 'log'){
-		expression = Math.log(expression);
-	}
-	else if(value == 'sin'){
-		expression = Math.sin(expression);
-	}
-	else if(value == 'cos'){
-		expression = Math.cos(expression);
-	}
-	else if(value == 'tan'){
-		expression = Math.tan(expression);
-	}
-
-	else if(value == '=') {
-		const answer = eval(expression);
-		expression = answer;
-
-	} else {
-		expression += value;
-	}
-
-	if(expression == undefined) {
-		expression = '';
-		ans.value = 0;
-	} else {
-		ans.value = expression;
-	}
-	// expression += value;
-
-
-}
-
-});
-const square =()=> {
-	return eval(expression*expression);
-}
+function Solve(val) {
+    var v = document.getElementById('res');
+    v.value += val;
+ }
+ function Result() {
+    var num1 = document.getElementById('res').value;
+    var num2 = eval(num1);
+    document.getElementById('res').value = num2;
+ }
+ function Clear() {
+    var inp = document.getElementById('res');
+    inp.value = '';
+ }
+ function Back() {
+    var ev = document.getElementById('res');
+    ev.value = ev.value.slice(0,-1);
+ }
